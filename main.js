2214167a7052affeec30f223f312c6d1462157df
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2023-02-19 23:53:47 +0000

    Add files via upload
    Fixed an uncaught reference error

--- main.js
+++ main.js
@@ -15,39 +15,6 @@ setTimeout(function () {
     $("#demo-modal").modal();
 }, 500);
 
-resultsHTML.onclick = function(event) {
-
-    const setValue = event.target.innerText;
-    autocomplete.value = setValue;
-    this.innerHTML = "";
-}
-
-function getResults(input){
-
-    const results = [];
-    for(i = 0; i < data.length; i++){
-
-        if(input == data[i].slice(0, input.length)){
-            results.push(data[i]);
-        }
-    }
-    return results;
-}
-
-autocomplete.oninput = function () {
-    let results = [];
-    const userInput = this.value;
-    resultsHTML.innerHTML = "";
-
-    if(userInput.length > 0){
-        results = getResults(userInput);
-        resultsHTML.style.display = "block";
-
-        for(i = 0; i < results.length; i++){
-            resultsHTML.innerHTML += "<li>" + results[i] + "</li>";
-        }
-    }
-};
 
 /**
  * this function uses the jQuery ajax method to get information
